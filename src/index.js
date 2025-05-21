61f469f3feaa37d630f4597d07d0a4942467fe29
kikiriki <kgutiiiii28@gmail.com>
2025-05-21 14:32:13 -0500

    Ejercicio

--- src/index.js
+++ src/index.js
@@ -1,23 +1,14 @@
-//Funcion para simular la descarga de un archivo, utilizando promesas
-
-function descargarArchivo() {
+function promesa() {
     return new Promise((resolve) => {
         setTimeout(() => {
-            resolve("📂 Archivo descargado con exito")
-        }, 5000);
+            resolve("💎 Promesa ejecutada con éxito después de 8 segundos")
+        }, 8000);
     })
 }
 
-//funcion con async
-async function ejecutar(){
-    try{
-        console.log("⏳ Descarga en proceso...")
-        const respuesta = await descargarArchivo()
-        console.log(respuesta)
-    } catch (error){
-        console.error("❌ Error al descargar", error)
 
-    }
-}
 
-ejecutar()
+
+
+
+
